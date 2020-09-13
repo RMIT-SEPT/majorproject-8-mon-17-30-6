2cf488f97bc165ee898bf2f9ed3fba097e9dc443
Leonardo <s3668709@student.rmit.edu.au>
2020-09-13 10:16:43 +1000

    include services

--- BackEnd/agme/src/main/java/com/rmit/sept/project/agme/model/ServiceType.java
+++ BackEnd/agme/src/main/java/com/rmit/sept/project/agme/model/ServiceType.java
@@ -1,4 +1,31 @@
 package com.rmit.sept.project.agme.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
 public class ServiceType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    public ServiceType(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    public ServiceType() {
+
+    }
+
 }
--- BackEnd/agme/src/main/java/com/rmit/sept/project/agme/repositories/ServiceTypeRepository.java
+++ BackEnd/agme/src/main/java/com/rmit/sept/project/agme/repositories/ServiceTypeRepository.java
@@ -1,4 +1,15 @@
 package com.rmit.sept.project.agme.repositories;
 
-public class ServiceTypeRepository {
+import com.rmit.sept.project.agme.model.ServiceType;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface ServiceTypeRepository extends CrudRepository<ServiceType, Long> {
+    @Override
+    Iterable<ServiceType> findAllById(Iterable<Long> iterable);
+
+    @Override
+    Iterable<ServiceType> findAll();
 }
