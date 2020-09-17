9b75c2a64e641b61d9d2fc1c392b7c42ba1b1960
Leonardo <s3668709@student.rmit.edu.au>
2020-09-18 07:23:00 +1000

    refactor init.sjh

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd BackEnd/agme
+sudo gpasswd -a $USER docker
+sudo dockerd
+cd /home/ec2-user/majorproject-8-mon-17-30-6/BackEnd/agme
+docker build . -t agme
+docker run -p 8080:8080 agme
