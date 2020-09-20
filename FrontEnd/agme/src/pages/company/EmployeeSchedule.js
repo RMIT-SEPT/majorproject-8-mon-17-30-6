e4924607dbc77d2f2a3faa101424e6b7d1c6b104
Leonardo <s3668709@student.rmit.edu.au>
2020-09-20 20:18:27 +1000

    add functionality to delete employee schedule

--- FrontEnd/agme/src/pages/company/EmployeeSchedule.js
+++ FrontEnd/agme/src/pages/company/EmployeeSchedule.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {EmployeeAvailability} from './EmployeeAvailability';
+const {deleteBooking} = require('../../mock/operations');
+
 export class EmployeeSchedule extends React.Component{
     constructor(props){
         super(props);
@@ -9,6 +11,7 @@ export class EmployeeSchedule extends React.Component{
         }
     }
 
+
     handleSelectDate(e){
         e.preventDefault();
         const value = e.target.value.split("-");
@@ -16,6 +19,7 @@ export class EmployeeSchedule extends React.Component{
     }
 
     render(){
+        console.log(this.props.bookings)
         if(this.props.employee.id){
             const bookings = this.props.employee.getBookings(this.props.bookings);
             const bookingsDictionary = {}
@@ -29,7 +33,7 @@ export class EmployeeSchedule extends React.Component{
                 bookingsDictionary[date].times.add(booking.time)
             });
             const times = bookingsDictionary[this.state.date] ? (bookingsDictionary[this.state.date].times) : new Set();
-            const availabilityComponent = this.state.date ? <EmployeeAvailability times={times}/> : "";
+            const availabilityComponent = this.state.date ? <EmployeeAvailability date={this.state.date} times={times} bookings={bookings} deleteBooking={this.props.deleteBooking}/> : "";
             return (
                 <div>
                     <div>
--- FrontEnd/agme/src/pages/company/ViewEmployees.js
+++ FrontEnd/agme/src/pages/company/ViewEmployees.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Employee from '../../model/Employee';
 import { EmployeeSchedule } from './EmployeeSchedule';
 import './viewEmployees.css'
-const {getCompanyEmployees} = require('../../mock/operations');
+const {getCompanyEmployees, deleteBooking} = require('../../mock/operations');
 //uncomment when integrating with API
 //const {getCompanyBookings} = require('../../mock/operations');
 
@@ -14,6 +14,9 @@ export class ViewEmployees extends React.Component{
             selectedEmployee: null,
             bookings:require('./mock/bookings.json')
         }
+
+
+
         getCompanyEmployees().then(response=>{
             if(response.statusCode===200){
                 const employees = response.body.map(employee=>{return new Employee(employee)});
@@ -29,8 +32,23 @@ export class ViewEmployees extends React.Component{
         //     }
         // });
 
-
+        this.deleteBooking = this.deleteBooking.bind(this);
         this.handleSelectEmployee = this.handleSelectEmployee.bind(this);
+    }
+
+    //need to delete here, otherwise children components wont re-render properly
+    deleteBooking(e){
+        e.preventDefault();
+        const bookingId = e.currentTarget.value;
+        deleteBooking(bookingId).then(response=>{
+            if(response.statusCode===204){
+                const bookings = this.state.bookings.filter(booking=>{
+                    return Number(booking.id)!==Number(bookingId);
+                });
+                this.setState({bookings:bookings})
+            }
+        })
+
     }
 
     handleSelectEmployee(e){
@@ -51,7 +69,7 @@ export class ViewEmployees extends React.Component{
                         <option value={'DEFAULT'} disabled>Select an employee</option>
                         {options}
                     </select>
-                    <EmployeeSchedule employee={new Employee(this.state.selectedEmployee)} bookings={this.state.bookings}/>
+                    <EmployeeSchedule deleteBooking={this.deleteBooking} employee={new Employee(this.state.selectedEmployee)} bookings={this.state.bookings}/>
                 </div>
             )
         }
