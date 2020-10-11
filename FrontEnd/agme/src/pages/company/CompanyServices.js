71f8866fd39c8cc8ba163c44970ad7abd43ebe62
Leonardo <s3668709@student.rmit.edu.au>
2020-10-11 14:55:48 +1100

    fix syntax issues

--- FrontEnd/agme/src/pages/company/CompanyServices.js
+++ FrontEnd/agme/src/pages/company/CompanyServices.js
@@ -19,7 +19,6 @@ export class CompanyServices extends React.Component{
                 const services = response.body.filter(service=>{
                     return service.company.filter(company=>{return company.username===username}).length>0
                 });
-                console.log(services)
                 this._isMounted &&this.setState({
                     done:true,
                     services: services
@@ -28,6 +27,9 @@ export class CompanyServices extends React.Component{
         });
     }
 
+    componentDidMount(){
+        this._isMounted = true;
+    }
     componentWillUnmount(){
         this._isMounted = false;
     }
