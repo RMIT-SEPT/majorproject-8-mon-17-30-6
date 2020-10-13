002a7a1f721804504c74ffd09f6225711b67f029
Leonardo <s3668709@student.rmit.edu.au>
2020-10-13 11:31:43 +1100

    remove code duplication

--- FrontEnd/agme/src/pages/company/AgmeServices.js
+++ FrontEnd/agme/src/pages/company/AgmeServices.js
@@ -3,72 +3,80 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter, Comparator } from 'react-bootstrap-table2-filter';
 import './services.css';
 import Spinner from 'react-bootstrap/Spinner';
-
+import {getDecodedJwtFromLocalStorage}  from "../../mock/operations/mock/functions/utils";//Add decode func
 const {apiCall} = require('../../mock/operations/mock/functions/operations');
 //To view list of services
 export default class AgmeServices extends React.Component{
     constructor(props){
-        super(props)
-        this.state = {services: this.props.services}
+        super(props);
+        this.state = {
+            services : null,
+            done: false
+        }
         this._isMounted = false;
+    }
+
+    componentDidMount(){
+        this._isMounted = true;
+        apiCall('company', 'getAllServices','','GET').then(response=>{
+            const username = getDecodedJwtFromLocalStorage().sub;
+            if(response.statusCode === 200){
+                let services = response.body;
+                if(this.props.filterByUsername){
+                    services = services.filter(service=>{
+                        return service.company.filter(company=>{return company.username===username}).length>0
+                    })
+                }
+                this._isMounted &&this.setState({
+                    done:true,
+                    services: services
+                })
+            }else{
+                this._isMounted &&this.setState({
+                    done:true,
+                    services: null
+                })
+            }
+        });
     }
 
     componentWillUnmount(){
         this._isMounted = false;
     }
 
-    componentDidMount(){
-        this._isMounted = true;
-        if(!this.props.services){
-            apiCall('company', 'getAllServices','','GET').then(response=>{
-                if(response.statusCode === 200){
-                    this._isMounted&&this.setState({
-                        services: response.body
-                    })
-                }
-            })
-        }
-    }
-
     render(){
-        if(!this.state.services){
-            return <div className="calling">
+        if(!this.state.done){
+            return (
             <div className="spinnerOutter">
-            <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-            </Spinner>
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading services...</span>
+                </Spinner>
             </div>
-            <br/>
-            <p>Please wait while we retrieve the services.</p>
-        </div>
+            );
         }else{
-            const rows = this.state.services.map(service=>{return {name:service.name,description:service.description}})
-            const columns = [{
-                dataField: 'name',
-                text: 'Service',
-                filter: textFilter({
-                  comparator: Comparator.LIKE
-                })
-              }, {
-                dataField: 'description',
-                text: 'Description',
-                filter: textFilter({
-                  comparator: Comparator.LIKE
-                })
-              }
-            ];
-        let servicesComponents =(
-            <div className="upcoming_appointments_table" >
-                <BootstrapTable keyField='id' data={ rows } columns={ columns } filter={ filterFactory() } />
-            </div>)
-        if(!(servicesComponents&&(servicesComponents.length))){
-            servicesComponents = "No services available";
-        }
-        return (
-            <div>
-                <h5>These are all services registered in AGME</h5>
-                {servicesComponents}
-            </div>
-        )
+            if(this.state.services&&this.state.services.length){
+                const rows = this.state.services.map(service=>{return {name:service.name,description:service.description}})
+                const columns = [{
+                    dataField: 'name',
+                    text: 'Service',
+                    filter: textFilter({
+                      comparator: Comparator.LIKE
+                    })
+                  }, {
+                    dataField: 'description',
+                    text: 'Description',
+                    filter: textFilter({
+                      comparator: Comparator.LIKE
+                    })
+                  }
+                ];
+                return (
+                    <div className="upcoming_appointments_table" >
+                        <BootstrapTable keyField='id' data={ rows } columns={ columns } filter={ filterFactory() } />
+                    </div>
+                );
+            }else{
+                return "No services available"
+            }
     }}
 }
--- FrontEnd/agme/src/pages/company/CompanyServices.js
+++ FrontEnd/agme/src/pages/company/CompanyServices.js
@@ -9,56 +9,14 @@ const {apiCall} = require('../../mock/operations/mock/functions/operations');
 export class CompanyServices extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            services: [],
-            done: false
-        }
-        this._isMounted = false;
-        apiCall('company', 'getAllServices','','GET').then(response=>{
-            const username = getDecodedJwtFromLocalStorage().sub;
-            if(response.statusCode === 200){
-                const services = response.body.filter(service=>{
-                    return service.company.filter(company=>{return company.username===username}).length>0
-                });
-                this._isMounted &&this.setState({
-                    done:true,
-                    services: services
-                })
-            }
-        });
-    }
-
-    componentDidMount(){
-        this._isMounted = true;
-    }
-    componentWillUnmount(){
-        this._isMounted = false;
     }
 
     render(){
-        if(!this.state.done){
-            return <div className="calling">
-            <div className="spinnerOutter">
-            <Spinner animation="border" role="status">
-                <span className="sr-only">Loading services...</span>
-            </Spinner>
-            </div>
-            <br/>
-            <p>Please wait while we retrieve the services.</p>
-        </div>
-        }else{
-            let servicesComponents;
-            if((!this.state.services)||(this.state.services.length===0)){
-                servicesComponents = "No services available";
-            }else{
-                servicesComponents = <AgmeServices services={this.state.services}/>;
-            }
         return (
             <div>
-                <h5>These are my services</h5>
-                {servicesComponents}
+                <h4>My services</h4>
+                <AgmeServices filterByUsername={true}/>
             </div>
         )
     }
-}
 }
