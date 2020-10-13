2f0ceb8443e997e6f5f6cd4453dcca36cb397bf7
Leonardo <s3668709@student.rmit.edu.au>
2020-10-13 11:32:52 +1100

    remove import

--- FrontEnd/agme/src/pages/company/CompanyServices.js
+++ FrontEnd/agme/src/pages/company/CompanyServices.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {getDecodedJwtFromLocalStorage}  from "../../mock/operations/mock/functions/utils";//Add decode func
 import './services.css'
-import Spinner from 'react-bootstrap/Spinner';
 import AgmeServices from './AgmeServices';
 
-const {apiCall} = require('../../mock/operations/mock/functions/operations');
 //To view list of services
 export class CompanyServices extends React.Component{
     constructor(props){
