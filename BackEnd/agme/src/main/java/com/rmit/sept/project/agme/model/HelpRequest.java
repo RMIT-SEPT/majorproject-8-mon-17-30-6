35b6579611f4df2d83bac600edce8fe2be0901c2
james <james.cw.ng@gmail.com>
2020-10-15 20:08:40 +1100

    Backend Report Issues to SysAdmin

--- BackEnd/agme/src/main/java/com/rmit/sept/project/agme/model/HelpRequest.java
+++ BackEnd/agme/src/main/java/com/rmit/sept/project/agme/model/HelpRequest.java
@@ -11,7 +11,6 @@ public class HelpRequest
     private String serviceName;
     private String message;
 
-
     public HelpRequest(){
 
     }
@@ -19,8 +18,17 @@ public class HelpRequest
     public void setUser(String user){
         this.user = user;
     }
-    public void setServiceName(String user){
-        this.user = user;
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setServiceName(String serviceName){
+        this.serviceName = serviceName;
+    }
+    public void setMessage(String message){
+        this.message = message;
     }
     public String getUser(){
         return this.user;
