cf57b87cf17111e5c38d8e61ee99fc046be3fbcc
Justin Seymour <jtseymour99@gmail.com>
2020-10-16 21:29:42 +1100

    Good enough

--- FrontEnd/agme/src/pages/company/AssignEmployee.js
+++ FrontEnd/agme/src/pages/company/AssignEmployee.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from "react-bootstrap/Button"
+import { apiCall } from '../../mock/operations/mock/functions/operations';
+const {apiCall, getDecodedJwtFromLocalStorage} = require('../../mock/operations/mock/functions/operations');
 
 export class AssignEmployee extends React.Component {
     
@@ -12,8 +14,43 @@ export class AssignEmployee extends React.Component {
             employeeID: "",
             bookingID: ""
         }
+        this._isMounted = false;
         this.addNewSubmit = this.addNewSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    addNewSubmit() {
+
+    }
+
+    handleInputChange(e) {
+
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+        apiCall('company', 'getBookings', null, 'GET').then(response=>{
+            const username = getDecodedJwtFromLocalStorage().sub;
+
+            if (response.statusCode == 200) {
+                let all = new Set();
+                let company = new Set();
+
+                const companyBookings = response.body.filter(booking=>{
+                    all.add(booking.bookingID);
+                    const hasBooking = booking.company.filter(company=>{return company.username===username}).length>0;
+
+                    if (hasBooking) {
+                        company.add(booking.bookingID);
+                    }
+                    return hasBooking;
+                });
+            }
+        });
     }
 
     render() {
